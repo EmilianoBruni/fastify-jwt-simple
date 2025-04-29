12a344ba15b3dcead4a558339942ce12df90fabe
Emiliano Bruni <info@ebruni.it>
2025-04-29 16:04:58 +0200

    Refactor tests for Fastify JWT plugin to improve readability and structure

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,27 +3,72 @@ import Fastify from 'fastify';
 import plugin from '@/index.js';
 
 t.test('Fastify plugin loading', async t => {
-    const fastify = Fastify();
+    const app = Fastify();
 
-    await fastify.register(plugin, {
+    await app.register(plugin, {
         secret: 'mysecret'
     });
 
     // exists fastify.jwt from @fastify/jwt
-    t.ok(fastify.jwt, 'fastify.jwt exists');
+    t.ok(app.jwt, 'fastify.jwt exists');
+    t.ok(app.fjs, 'app.fjs exists');
+    t.ok(app.fjs.jwtBannedToken, 'app.fjs.jwtBannedToken exists');
+    t.ok(app.fjs.jwtBannedRefresh, 'app.fjs.jwtBannedRefresh exists');
+    t.ok(app.fjs.isToAuthenticate, 'app.fjs.isToAuthenticate exists');
+    t.ok(app.fjs.userData, 'app.fjs.userData exists');
+    t.equal(
+        typeof app.fjs.isToAuthenticate,
+        'function',
+        'app.fjs.isToAuthenticate is a function'
+    );
+    t.equal(
+        typeof app.fjs.userData,
+        'function',
+        'app.fjs.userData is a function'
+    );
 
-    fastify.get('/', async (req, rep) => {
-        // check if fastify.jwtBannedToken exists
-        t.ok(req.jwtBannedToken, 'req.jwtBannedToken exists');
-        // check if fastify.jwtBannedRefresh exists
-        t.ok(req.jwtBannedRefresh, 'req.jwtBannedRefresh exists');
+    await app.close();
+});
+
+t.test('@fastify/jwt loaded before fastify-jwt-simple', async () => {
+    const app = Fastify();
+
+    await app.register(import('@fastify/jwt'), {
+        secret: 'mysecret'
+    });
+
+    await app.register(plugin, {
+        secret: 'anothersecret'
+    });
+    await app.close();
+});
+
+t.test('isAuthenticate function', async t => {
+    const app = Fastify();
+    await app.register(plugin, {
+        secret: 'mysecret'
+    });
+
+    app.get('/', async () => {
         return { test: true };
     });
 
-    const response = await fastify.inject('/');
-    t.equal(response.statusCode, 200, 'response status code is 200');
+    // by default all routes require authentication so 403
+    const response = await app.inject('/');
+    t.equal(response.statusCode, 403, 'response status code is 403');
     const payload = JSON.parse(response.payload);
-    t.equal(payload.test, true, 'response payload is correct');
+    t.equal(payload.error, 'Forbidden', 'response payload is correct');
 
-    await fastify.close();
+    // test isToAuthenticate to bypass authentication
+    app.fjs.isToAuthenticate = async () => {
+        return false;
+    };
+
+    // now the route should return 200
+    const response2 = await app.inject('/');
+    t.equal(response2.statusCode, 200, 'response status code is 200');
+    const payload2 = JSON.parse(response2.payload);
+    t.equal(payload2.test, true, 'response payload is correct');
+
+    await app.close();
 });
